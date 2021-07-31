7f6de259300a9fd3630f4697e6b5605ba18b0a1b
Luk Krzy <lukaskrzy@gmail.com>
2021-07-31 11:09:10 +0200

    add students column

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,22 +7,10 @@ import { PageNotFoundComponent } from './components/page-not-found/page-not-foun
 const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'home', redirectTo: ''},
-  {
-    path: 'login', loadChildren:
-      () => import('./module/auth/login.module').then(m => m.LoginModule)
-  },
-  {
-    path: 'calendar', loadChildren:
-      () => import('./module/calendar/calendar.module').then(m => m.CalendarModule), canActivate: [AuthGuard]
-  },
-  {
-    path: 'products', loadChildren:
-      () => import('./module/products/products.module').then(m => m.ProductsModule), canActivate: [AuthGuard]
-  },
-  {
-    path: 'students', loadChildren:
-      () => import('./module/students/students.module').then(m => m.StudentsModule)
-  },
+  {path: 'login', loadChildren: () => import('./module/auth/login.module').then(m => m.LoginModule)},
+  {path: 'calendar', loadChildren: () => import('./module/calendar/calendar.module').then(m => m.CalendarModule), canActivate: [AuthGuard]},
+  {path: 'products', loadChildren: () => import('./module/products/products.module').then(m => m.ProductsModule), canActivate: [AuthGuard]},
+  {path: 'students', loadChildren: () => import('./module/students/students.module').then(m => m.StudentsModule)},
   {path: '**', component: PageNotFoundComponent}
 ];
 
--- src/app/module/students/students.module.ts
+++ src/app/module/students/students.module.ts
@@ -2,6 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { StudentsRoutingModule } from './students-routing.module';
 import { StudentsComponent } from './students.component';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatTableModule } from '@angular/material/table';
+import { FlexModule } from '@angular/flex-layout';
+import { MatSortModule } from '@angular/material/sort';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   declarations: [
@@ -9,7 +15,13 @@ import { StudentsComponent } from './students.component';
   ],
   imports: [
     CommonModule,
-    StudentsRoutingModule
+    StudentsRoutingModule,
+    MatPaginatorModule,
+    MatTableModule,
+    FlexModule,
+    MatSortModule,
+    MatFormFieldModule,
+    MatInputModule
   ]
 })
 export class StudentsModule {
