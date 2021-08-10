f77e4bf7e38d32beeaa4b32cf83fa7c282eb722c
Luk Krzy <lukaskrzy@gmail.com>
2021-08-10 11:25:30 +0200

    Work on date to add Lesson

---
+++ src/app/module/students/student-details/add-lesson/add-lesson.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-add-lesson',
+  templateUrl: './add-lesson.component.html',
+  styleUrls: ['./add-lesson.component.scss']
+})
+export class AddLessonComponent implements OnInit {
+  instructors: string[] = ['jan', 'Laszalo', 'Anna', 'Zdiosiek'];
+  hours = [...Array.from({length: 14}, (_, i) => i + 7)];
+  hoursCount = [...Array.from({length: 5}, (_, i) => i + 1)];
+  newLesson: FormGroup = new FormGroup({});
+
+  constructor(private fb: FormBuilder,) {
+  }
+
+  get date(): FormControl {
+    return this.newLesson.get('date') as FormControl;
+  }
+
+  ngOnInit(): void {
+    this.initLessonForm();
+  }
+
+  onSave() {
+    console.log(this.newLesson.value);
+    console.log(this.newLesson.value.date._d.toISOString());
+  }
+
+  validateForm(): boolean {
+    return false;
+  }
+
+  private initLessonForm(): void {
+    this.newLesson = this.fb.group({
+      instructorId: [null, Validators.required],
+      date: [new Date().toISOString, Validators.required],
+      hour: [null, Validators.required],
+      hoursCount: [null, Validators.required],
+    });
+  }
+}
--- src/app/module/students/student-list.module.ts
+++ src/app/module/students/student-list.module.ts
@@ -5,23 +5,25 @@ import { StudentsListComponent } from './students-list.component';
 import { MaterialModule } from '../../shared/material/material.module';
 import { StudentDetailsComponent } from './student-details/student-details.component';
 import { LayoutModule } from '@angular/cdk/layout';
-import { MatRippleModule } from '@angular/material/core';
 import { StudentAccountComponent } from './student-details/student-account/student-account.component';
 import { StudentProductsComponent } from './student-details/student-products/student-products.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { AddLessonComponent } from './student-details/add-lesson/add-lesson.component';
 
 @NgModule({
   declarations: [
     StudentsListComponent,
     StudentDetailsComponent,
     StudentAccountComponent,
-    StudentProductsComponent
+    StudentProductsComponent,
+    AddLessonComponent
   ],
   imports: [
     CommonModule,
     StudentsRoutingModule,
     MaterialModule,
     LayoutModule,
-    MatRippleModule,
+    ReactiveFormsModule,
   ]
 })
 export class StudentListModule {
