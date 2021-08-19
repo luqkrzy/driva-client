b5bc202d69e31535338f310cdb87432ca41b6395
Luk Krzy <lukaskrzy@gmail.com>
2021-08-19 12:27:06 +0200

    Add add lesson component

---
+++ src/app/module/student-details/update-lesson/update-lesson.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-update-lesson',
+  templateUrl: './update-lesson.component.html',
+  styleUrls: ['./update-lesson.component.scss']
+})
+export class UpdateLessonComponent implements OnInit {
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+}
+
+
