21bc538eff81579da2688053b40162be4aea8f0e
Luk Krzy <lukaskrzy@gmail.com>
2021-08-29 14:37:23 +0200

    Add component add-product-type.component.ts

---
+++ src/app/module/product-type/add-product-type/add-product-type.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-product-type',
+  templateUrl: './add-product-type.component.html',
+  styleUrls: ['./add-product-type.component.scss']
+})
+export class AddProductTypeComponent implements OnInit {
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+}
