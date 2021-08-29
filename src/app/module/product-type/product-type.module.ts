9c6814b8b79a9d7458a6db2a749302ddd256cbbe
Luk Krzy <lukaskrzy@gmail.com>
2021-08-29 14:38:19 +0200

    Add component update-product-type.component.ts

--- src/app/module/product-type/product-type.module.ts
+++ src/app/module/product-type/product-type.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 import { ProductTypeRoutingModule } from './product-type-routing.module';
 import { ProductTypeComponent } from './product-type.component';
 import { AddProductTypeComponent } from './add-product-type/add-product-type.component';
+import { UpdateProductTypeComponent } from './update-product-type/update-product-type.component';
 
 @NgModule({
   declarations: [
     ProductTypeComponent,
-    AddProductTypeComponent
+    AddProductTypeComponent,
+    UpdateProductTypeComponent
   ],
   imports: [
     CommonModule,
---
+++ src/app/module/product-type/update-product-type/update-product-type.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-update-product-type',
+  templateUrl: './update-product-type.component.html',
+  styleUrls: ['./update-product-type.component.scss']
+})
+export class UpdateProductTypeComponent implements OnInit {
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+}
